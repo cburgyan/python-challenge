c32b7d1ff296f4c9b7c218bf5f81ea5ac1242b2c
cburgyan <cburgyan@gmail.com>
2023-06-27 22:47:12 -0500

    remove erroneous month function and add useful month function, AddOnlyAValidMonth

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,25 @@
 import csv
 import os
 
-def GetTotalNumberOfMonths(budgetReader):
-    setOfMonths = set()
-    for row in budgetReader:
-        setOfMonths.add(row[0][:3])
-    print(setOfMonths)
-    return len(setOfMonths)
+monthsList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+def AddOnlyAValidMonth(row):
+    # Check to make sure row has valid data
+    if len(row) != 0:
+        if row[0][:3] in monthsList:
+            return 1
+        else:
+            # perhaps a month is just mispelled (eg "Jna" instead of "Jan")
+            rowIsValid = input(f'Does this, "{row[0]}" have a valid month? (y)es or (n)o ')
+            if rowIsValid == 'y':
+                return 1
+            
+    return 0
 
 
 
 csv_filepath = os.path.join(".", "Resources", "budget_data.csv")
 
-monthsList = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 
 with open(csv_filepath, 'r') as rFile:
@@ -22,16 +29,8 @@ with open(csv_filepath, 'r') as rFile:
     print("hello")
     numMonths = 0
     for row in budgetReader:
-
-        # Check to make sure row has valid data
-        if len(row) != 0:
-            if row[0][:3] in monthsList:
-                numMonths += 1
-            else:
-                # perhaps a month is just mispelled (eg "Jna" instead of "Jan")
-                rowIsValid = input(f'Does this, "{row[0]}" have a valid month? (y)es or (n)o ')
-                if rowIsValid == 'y':
-                    numMonths += 1
+        numMonths += AddOnlyAValidMonth(row)
+        
         
 
         
